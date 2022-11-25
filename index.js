71ceb639c374a49d520f3ecefd5dfaa068b8d3e6
Miguel Ramírez <100735523+Migue04fuentes@users.noreply.github.com>
2022-11-25 12:07:57 -0500

    calculo de intervalos

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
-let morning_start = document.getElementById('hora1').value.split(':');
-let morning_end = document.getElementById('hora2').value.split(':');
-
-let h1 = new Date();
-let h2 = new Date();
-let ht = new Date();
-
-
-h1.setHours(morning_start[0],morning_start[1]);
-h2.setHours(morning_end[0],morning_end[1]);
+// Solo números en los input
+function solo_numeros(e) {
+    let keys = e.key;
+    return keys >= 0 && keys <= 9;
+}
 
 
-ht.setHours(h2.getHours()-h1.getHours(),h2.getMinutes()-h1.getMinutes(),h2.getSeconds()-h1.getSeconds());
-let minutes = ((h2.getHours()-h1.getHours())*60)+h2.getMinutes()-h1.getMinutes();
-
-let resultado = document.getElementById('resultado');
-resultado.innerHTML = "Total de horas: " + (ht.getHours() ? ht.getHours() + (ht.getHours() > 1 ? " horas":"hora"): "") +"" +(ht.getMinutes() ? ht.getMinutes()+(ht.getMinutes() > 1 ? " Minutos":" Minuto"): "");
-
-
+// input de cupos
+let num = "";
 let cupos = document.getElementById('cupos');
-cupos.addEventListener('keyup', function(valor){
+cupos.addEventListener('keypress', function (valor) {
+    if (!solo_numeros(valor)) {
+        valor.preventDefault();
+    }
 
-      minutes *=valor.key;
-      console.log(minutes);
-      document.getElementById('intervalos').value = minutes;
+    if (valor.key >= 0 && valor.key <= 9) {
+        num += valor.key;
 
-})
+        let morning_start = document.getElementById('hora1').value.split(':');
+        let morning_end = document.getElementById('hora2').value.split(':');
+       
+        let h1 = new Date();
+        let h2 = new Date();
+        let ht = new Date();
 
+
+        h1.setHours(morning_start[0], morning_start[1]);
+        h2.setHours(morning_end[0], morning_end[1]);
+
+
+        ht.setHours(h2.getHours() - h1.getHours(), h2.getMinutes() - h1.getMinutes(), h2.getSeconds() - h1.getSeconds());
+        let minutes = ((h2.getHours() - h1.getHours()) * 60) + h2.getMinutes() - h1.getMinutes();
+        
+        minutes /= num;
+        document.getElementById('intervalos').value = minutes;
+
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = "Total de horas: " + (ht.getHours() ? ht.getHours() + (ht.getHours() > 1 ? " horas" : "hora") : "") + "" + (ht.getMinutes() ? ht.getMinutes() + (ht.getMinutes() > 1 ? " Minutos" : " Minuto") : "");
+    }
+});
+
+cupos.addEventListener('keyup', function(event){
+    if(event.key == "Backspace"){
+        num = num.substring(0, num.length -1);
+        console.log(num);
+    }
+});
+
+
+// input de intervalos
+let intervalos = document.getElementById('intervalos');
+intervalos.addEventListener('keypress', function(e){
+    if(!solo_numeros(e)){
+        e.preventDefault();
+    }
+});
+
+
+
+/* 
 
 var hora1 = ("04:29:01").split(":"),
     hora2 = ("03:28:56").split(":"),
     t1 = new Date(),
     t2 = new Date();
- 
+
 t1.setHours(hora1[0], hora1[1], hora1[2]);
 t2.setHours(hora2[0], hora2[1], hora2[2]);
 console.log(t1);
@@ -39,4 +72,4 @@ console.log(t1);
 t1.setHours(t1.getHours() - t2.getHours(), t1.getMinutes() - t2.getMinutes(), t1.getSeconds() - t2.getSeconds());
 //Imprimo el resultado
 // let resultado = document.getElementById('resultado');
-resultado.innerHTML += "\n La diferencia es de: " + (t1.getHours() ? t1.getHours() + (t1.getHours() > 1 ? " horas" : " hora") : "") + (t1.getMinutes() ? ", " + t1.getMinutes() + (t1.getMinutes() > 1 ? " minutos" : " minuto") : "") + (t1.getSeconds() ? (t1.getHours() || t1.getMinutes() ? " y " : "") + t1.getSeconds() + (t1.getSeconds() > 1 ? " segundos" : " segundo") : "");
+resultado.innerHTML += "\n La diferencia es de: " + (t1.getHours() ? t1.getHours() + (t1.getHours() > 1 ? " horas" : " hora") : "") + (t1.getMinutes() ? ", " + t1.getMinutes() + (t1.getMinutes() > 1 ? " minutos" : " minuto") : "") + (t1.getSeconds() ? (t1.getHours() || t1.getMinutes() ? " y " : "") + t1.getSeconds() + (t1.getSeconds() > 1 ? " segundos" : " segundo") : ""); */
