7d3c04cf7abc6b3cbd6ffcee72160883e3a280e6
Miguel Ramírez <100735523+Migue04fuentes@users.noreply.github.com>
2022-12-18 21:19:05 -0500

    calculo h final y por cambio de hora

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ let turno_mat = document.getElementById("switch_label");
 let turno_vesp = document.getElementById("switch_turno_vesp");
 
 // HORAS TURNO MAÑANA
+let mornings = document.getElementById("hora1");
+let morninge = document.getElementById("hora2");
 let morning_start = document.getElementById("hora1").value;
 let morning_end = document.getElementById("hora2").value;
+
+//  HORAS TURNO VESPERTINO
+let afternoon_start = document.getElementById("horat1").value;
+let afternoon_end = document.getElementById("horat2").value;
 
 // input de cupos
 let num = "";
@@ -598,18 +604,31 @@ function KeyPressInt(e) {
 }
 cupos.onkeydown = KeyPressCupo;
 
-/* 
+// Cálculo de hora final
+let hora_final = document.getElementById("btn_hora_final");
+hora_final.addEventListener("click", () => {
+  if (turno_mat.checked && turno_vesp.checked === false) {
+    morning_start = document.getElementById("hora1").value;
+    console.log(morning_start);
+  }
+  if (turno_mat.checked === false && turno_vesp.checked) {
+    afternoon_start = document.getElementById('horat1').value;
+    console.log(afternoon_start);
+  }
+  if(turno_mat.checked && turno_vesp.checked){
+    console.log('Este es más complicado');
+  }
+});
 
-var hora1 = ("04:29:01").split(":"),
-    hora2 = ("03:28:56").split(":"),
-    t1 = new Date(),
-    t2 = new Date();
 
-t1.setHours(hora1[0], hora1[1], hora1[2]);
-t2.setHours(hora2[0], hora2[1], hora2[2]);
-console.log(t1);
-//Aquí hago la resta
-t1.setHours(t1.getHours() - t2.getHours(), t1.getMinutes() - t2.getMinutes(), t1.getSeconds() - t2.getSeconds());
-//Imprimo el resultado
-// let resultado = document.getElementById('resultado');
-resultado.innerHTML += "\n La diferencia es de: " + (t1.getHours() ? t1.getHours() + (t1.getHours() > 1 ? " horas" : " hora") : "") + (t1.getMinutes() ? ", " + t1.getMinutes() + (t1.getMinutes() > 1 ? " minutos" : " minuto") : "") + (t1.getSeconds() ? (t1.getHours() || t1.getMinutes() ? " y " : "") + t1.getSeconds() + (t1.getSeconds() > 1 ? " segundos" : " segundo") : ""); */
+// CÁLCULOS POR CAMBIOS DE HORA 
+// hora inicio turno mañana
+mornings.addEventListener('change', ()=>{
+  console.log('Se ha detectado un cambio him');
+});
+
+// Hora final turno mañana
+morninge.addEventListener('change', () => {
+  console.log('Se ha detectado un cambio hfm');
+});
+
